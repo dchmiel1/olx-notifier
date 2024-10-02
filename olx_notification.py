755437c71eb2ec46a8ed5b8e3c5ecc33be02e0bb
dchmiel1 <chmielu.99@wp.pl>
2024-10-02 20:26:33 +0200

    Init

---
+++ olx_notification.py
@@ -0,0 +1,24 @@
+from twilio.rest import Client
+
+# in this part you have to replace account_sid
+# auth_token, twilio_number, recipient_number with your actual credential
+
+account_sid = 'ACa5f316db44cd623625218d50111b9e85'
+auth_token = 'd654ebff5a91021b065f68d255df1c7e'
+twilio_number = '+13343103088'
+recipient_number = '+48602460473'
+
+# Create Twilio client
+client = Client(account_sid, auth_token)
+
+# Send SMS
+# in body part you have to write your message
+message = client.messages.create(
+    body='This is a new message',
+    from_=twilio_number,
+    to=recipient_number
+)
+
+print(f"Message sent with SID: {message.sid}")
+
+https://www.olx.pl/dom-ogrod/meble/grojec/q-oddam/?search%5Bdist%5D=75&search%5Border%5D=created_at:desc
