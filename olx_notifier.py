64c56d2693b95921157cfc79caa785b28f4a106e
dchmiel1 <chmielu.99@wp.pl>
2024-10-15 17:59:33 +0200

    Add logging

--- olx_notifier.py
+++ olx_notifier.py
@@ -1,5 +1,8 @@
+import datetime
+
 import schedule
 
+from log import logger
 from parser import OlxPageParser
 from scraper import fetch_offers_page
 from sms import SMSClient
@@ -8,9 +11,15 @@ from time import sleep
 
 
 def init_offers():
+    logger.info("Initializing olx notifier...")
     page_html = fetch_offers_page()
     offers = OlxPageParser(page_html).retrieve_offers()
+    storage = Storage()
+    ids = storage.read_ids()
+    offers = retrieve_new_offers(offers, ids)
+    logger.info("Fetched %d initial offers", len(offers))
     Storage().save_offers(offers)
+    logger.info("Initial offers saved")
 
 
 def retrieve_new_offers(offers, old_ids):
@@ -22,20 +31,29 @@ def retrieve_new_offers(offers, old_ids):
 
 
 def check_for_updates():
+    logger.info("\n\n")
+    logger.info("########")
+    logger.info(datetime.datetime.now())
+    logger.info("########")
+    logger.info("Checking for updates...")
+
     page_html = fetch_offers_page()
     offers = OlxPageParser(page_html).retrieve_offers()
+    logger.info("Fetched %d offers", len(offers))
     storage = Storage()
     sms_client = SMSClient()
     ids = storage.read_ids()
 
     offers = retrieve_new_offers(offers, ids)
-    print("New offers: ", len(offers))
+    logger.info("New offers: %s", len(offers))
     sms_client.notify_about_new_offers(offers)
     storage.save_offers(offers)
+    logger.info("New offers saved")
 
 
 if __name__ == "__main__":
     init_offers()
+
     schedule.every(10).minutes.do(check_for_updates)
 
     while True:
